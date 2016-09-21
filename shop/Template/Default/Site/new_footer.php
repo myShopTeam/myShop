289cbbfd8cc8f1d470e97275f5d26878089d459f
LinPei <535201470@qq.com>
2016-09-21 23:17:56 +0800

    企业站公共部分，首页

--- shop/Template/Default/Site/new_footer.php
+++ shop/Template/Default/Site/new_footer.php
@@ -7,17 +7,13 @@
             <a class="idea" href="#"></a><span>|</span>
             <a class="case" href="#"></a><span>|</span>
             <a class="ser" href="#"></a>-->
-            <a href="#">网站首页</a><span>|</span>
-            <a href="#">公司介绍</a><span>|</span>
-            <a href="#">新闻中心</a><span>|</span>
-            <a href="#">产品中心</a><span>|</span>
-            <a href="#">招商合作</a><span>|</span>
-            <a href="#">救援培训</a><span>|</span>
-            <a href="#">服务卡激活</a><span>|</span>
-            <a href="#">人才招聘</a><span>|</span>
-            <a href="#">联系我们</a>
+            <content action="category" catid="1" order="listorder asc">
+                <volist name="data" id="vo" key="k">
+                    <a href="<if condition="$vo['menuurl'] neq ''">{$vo.menuurl}<else/>{$vo.url}</if>">{$vo.catname}<if condition="$k neq count($data)"><span>|</span></if></a>
+                </volist>
+            </content>
         </p>
-        <p class="CopyRight"><span>湖北国安家园救援有限责任公司 版权所有 2011-2012 &nbsp;&nbsp;&nbsp; 鄂ICP备10232294号 </span><img src="{$site_info.enterprise_path}images/cnzz_pic.gif" /></p>
+        <p class="CopyRight"><span>{:cache('Config.company')} {:cache('Config.copyright')} &nbsp;&nbsp;&nbsp;&nbsp; {:cache('Config.put_on_records')} </span><img src="{$site_info.enterprise_path}images/cnzz_pic.gif" /></p>
     </div>
 </div>
 <script type="text/javascript" src="{$site_info.enterprise_path}script/jquery.js"></script>
